6315e9de208d54b27fb0792e94952a648d72ff80
Kevin Pessetto <kjpessetto@gmail.com>
2019-06-24 18:29:10 -0500

    Added dummy stripe transaction

--- server/index.js
+++ server/index.js
@@ -6,10 +6,13 @@ const authController = require('./controllers/authController');
 const adminController = require('./controllers/adminController')
 const userController = require('./controllers/userController')
 const stripe = require("stripe")("sk_test_rQjptKVen4my7b1D9ilzKtk500biiETnqV")
+const uuid = require("uuid/v4");
+const cors = require("cors");
 
 const app = express();
 
 app.use(express.json()); //app.use is for middleware
+app.use(cors());
 app.use(require("body-parser").text());
 app.use(session({
     secret: process.env.SESSION_SECRET,
@@ -48,5 +51,50 @@ app.post('/user/transactions', userController.addTransaction)
 app.put('/user/:id', userController.updateEmail)
 app.delete('/user/:id', userController.deleteAccount)
 app.post('/charge', userController.chargeCard)
+app.post("/checkout", async (req, res) => {
+    console.log("Request:", req.body);
+  
+    let error;
+    let status;
+    try {
+      const { product, token } = req.body;
+  
+      const customer = await stripe.customers.create({
+        email: token.email,
+        source: token.id
+      });
+  
+      const idempotency_key = uuid();
+      const charge = await stripe.charges.create(
+        {
+          amount: product.price * 100,
+          currency: "usd",
+          customer: customer.id,
+          receipt_email: token.email,
+          description: `Purchased the ${product.name}`,
+          shipping: {
+            name: token.card.name,
+            address: {
+              line1: token.card.address_line1,
+              line2: token.card.address_line2,
+              city: token.card.address_city,
+              country: token.card.address_country,
+              postal_code: token.card.address_zip
+            }
+          }
+        },
+        {
+          idempotency_key
+        }
+      );
+      console.log("Charge:", { charge });
+      status = "success";
+    } catch (error) {
+      console.error("Error:", error);
+      status = "failure";
+    }
+  
+    res.json({ error, status });
+  });
 
 app.listen(5050, () => console.log(`Listening on Port 5050`));
--- src/components/CheckoutForm.js
+++ src/components/CheckoutForm.js
@@ -1,35 +1,91 @@
-import React, {Component} from 'react';
-import {CardElement, injectStripe} from 'react-stripe-elements';
+import React from "react";
+import StripeCheckout from "react-stripe-checkout";
+import axios from "axios";
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css"
+import ReactDOM from "react-dom";
 
-class CheckoutForm extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {complete: false};
-    this.submit = this.submit.bind(this);
-  }
+toast.configure();
 
-  async submit(ev) {
-    let {token} = await this.props.stripe.createToken({name: "Name"});
-    let response = await fetch("/charge", {
-      method: "POST",
-      headers: {"Content-Type": "text/plain"},
-      body: token.id
-    });
-  
-    if (response.ok) this.setState({complete: true}) 
-    console.log("Purchase Complete!")
-  }
+export default function CheckoutForm() {
+  const [product] = React.useState({
+    name: "Deposit funds",
+    price: 64998.67,
+    description: "Cool car"
+  });
 
-  render() {
-      if (this.state.complete) return <h1>Deposit Complete</h1>
-    return (
-      <div className="checkout">
-        <p>Would you like to complete the deposit?</p>
-        <CardElement />
-        <button onClick={this.submit}>Deposit</button>
-      </div>
+  async function handleToken(token, addresses) {
+    const response = await axios.post(
+      "/checkout",
+      { token, product }
     );
+    const { status } = response.data;
+    console.log("Response:", response.data);
+    if (status === "success") {
+      toast("Success! Check email for details", { type: "success" });
+    } else {
+      toast("Something went wrong", { type: "error" });
+    }
   }
+
+  return (
+    <div className="container">
+      <div className="product">
+        <h1>{product.name}</h1>
+        <h3>${product.price}</h3>
+      </div>
+      <StripeCheckout
+        stripeKey="pk_test_2QtpeVuM2hru92q1zHxaZQFK002dx0OCJ1"
+        token={handleToken}
+        amount={product.price * 100}
+        name="Tesla Roadster"
+        billingAddress
+        shippingAddress
+      />
+    </div>
+  );
 }
 
-export default injectStripe(CheckoutForm);
+const rootElement = document.getElementById("root");
+ReactDOM.render(<CheckoutForm />, rootElement);
+
+
+
+
+
+// import React, {Component} from 'react';
+// import {CardElement, injectStripe} from 'react-stripe-elements';
+// import {Button} from 'reactstrap'
+
+// class CheckoutForm extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {complete: false};
+//     this.submit = this.submit.bind(this);
+//   }
+
+//   async submit(ev) {
+//     let {token} = await this.props.stripe.createToken({name: "Name"});
+//     let response = await fetch("/charge", {
+//       method: "POST",
+//       headers: {"Content-Type": "text/plain"},
+//       body: token.id
+//     });
+  
+//     if (response.ok) this.setState({complete: true}) 
+//     console.log("Purchase Complete!")
+//   }
+
+//   render() {
+//       if (this.state.complete) return <h1>Deposit Complete</h1>
+//     return (
+//       <div className="checkout">
+//         <p>Would you like to complete the deposit?</p>
+//         <CardElement />
+//         <Button onClick={this.submit}>Deposit</Button>
+//       </div>
+//     );
+//   }
+// }
+
+// export default injectStripe(CheckoutForm);
